63a008bb718850825a71ababf08ba55c99226632
redfast00 <redfast00@users.noreply.github.com>
2016-03-13 13:24:22 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+var port = 8080;
+var http = require('http');
+var URL = require('url');
+var server = http.createServer(function(req, res) {
+    var query = unescape(URL.parse(req.url).pathname).substring(1);
+    var dateToParse;
+    if (Number(query)) {
+        //query is a unix timestamp
+        dateToParse = new Date(Number(query) * 1000);
+    }
+    else if (Date.parse(query)){
+        //query is readable
+        dateToParse = new Date(query);
+    }
+    else if (! query){
+        res.end('This is a timeservice endpoint. I wrote this as a FreeCodeCamp baseline');
+        return true;
+    }
+    else {
+        res.writeHead(200, {'Content-Type': 'text/json'});
+        res.end(JSON.stringify({'unix': null, 'natural': null}));
+        return true;
+    }
+    
+    res.writeHead(200, {'Content-Type': 'text/json'});
+    res.end(JSON.stringify({'unix': dateToParse.getTime() / 1000, 'natural': dateToParse.toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: 'numeric' })}));
+});
+server.listen(port);
