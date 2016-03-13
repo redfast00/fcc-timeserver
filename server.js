63c2bff1b504db14f99507df569e9e4e4a6a1a48
redfast00 <redfast00@users.noreply.github.com>
2016-03-13 13:47:52 +0100

    a

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-var port = 80;
-var http = require('http');
+var https = require('https');
 var URL = require('url');
-var server = http.createServer(function(req, res) {
+var server = https.createServer(function(req, res) {
     var query = unescape(URL.parse(req.url).pathname).substring(1);
     var dateToParse;
     if (Number(query)) {
@@ -25,4 +24,4 @@ var server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'application/json'});
     res.end(JSON.stringify({'unix': dateToParse.getTime() / 1000, 'natural': dateToParse.toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: 'numeric' })}));
 });
-server.listen(port);
+server.listen(process.env.PORT || 5000);
